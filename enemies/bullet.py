57418d78b89c2bd6355e01437315874e5d24a181
Vlad Yudin <vladislav.yudin.200@gmail.com>
2022-01-16 11:04:48 +0800

    Created class bullet

---
+++ enemies/bullet.py
@@ -0,0 +1,57 @@
+import os
+import random
+
+import pygame
+
+from enemies import enemy
+
+WIDTH, HEIGHT = 800, 600
+FLOOR = 500
+FPS = 60
+
+
+class Bullet(enemy.Enemy):
+    """Класс пули"""
+
+    def __init__(self, screen):
+        super().__init__(screen, "data/bullet.png", (80, 30))
+        # Где появится пуля. Справа (0), слева (1)
+        self.side = random.randrange(0, 2)
+
+        if self.side == 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+
+        self.generate_position()
+
+        # Скорость пули
+        self.speed = 10
+
+    def generate_position(self):
+        if self.side == 0:
+            # Справа.
+
+            self.rect.x = WIDTH
+            self.rect.y = random.randrange(FLOOR - 250, FLOOR - 35)
+        elif self.side == 1:
+            # Слева.
+
+            self.rect.x = 0 - self.rect.size[0]
+            self.rect.y = random.randrange(FLOOR - 250, FLOOR - 35)
+
+    def can_move(self) -> bool:
+        if self.side == 0:
+            # Справа налево.
+            return self.rect.x > (0 - self.rect.size[0])
+        elif self.side == 1:
+            # Слева направо.
+            return self.rect.x < WIDTH
+
+    def move(self) -> None:
+        if self.side == 0:
+            # Справа налево.
+            self.rect.x -= self.speed
+        elif self.side == 1:
+            # Слева направо
+            self.rect.x += self.speed
+        pygame.draw.rect(self.screen, (255, 0, 0),
+                         (50, 50, 200, 200))
